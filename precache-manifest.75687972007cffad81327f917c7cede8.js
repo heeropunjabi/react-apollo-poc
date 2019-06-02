ea31c96fe707175d761274ccc21922298babe9da
heeropunjabi <heeropunjabi@gmail.com>
2019-06-02 14:09:16 +0530

    Updates

---
+++ precache-manifest.75687972007cffad81327f917c7cede8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d23c40f5034506fc67675a3fc7f5be",
+    "url": "/react-apollo-poc/index.html"
+  },
+  {
+    "revision": "4388505fe0654129c6be",
+    "url": "/react-apollo-poc/static/css/main.56c7eab6.chunk.css"
+  },
+  {
+    "revision": "8948746d54778d6b0958",
+    "url": "/react-apollo-poc/static/js/2.2ff19c3f.chunk.js"
+  },
+  {
+    "revision": "4388505fe0654129c6be",
+    "url": "/react-apollo-poc/static/js/main.5c38ad65.chunk.js"
+  },
+  {
+    "revision": "bc03a37dea3f84da4c6e",
+    "url": "/react-apollo-poc/static/js/runtime~main.5df336de.js"
+  }
+]);
